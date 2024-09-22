210dfbd9f5f6b2ab377a18c97f5c64731402644c
rcm71 <rcm71@pitt.edu>
2024-09-22 15:11:28 -0400

    Frame for Hellow World

--- Cuscuta/src/main.rs
+++ Cuscuta/src/main.rs
@@ -1,3 +1,73 @@
+use bevy::{prelude::*, window::PresentMode};
+// dependencies
+
+#[derive(Component, Deref, DerefMut)]
+struct PopupTime(Timer);
+// Timers
+
+// Little hello_world credit scene.
+// Uses Sprite Bundle with each image spawned in a stack on z axis
+// Timer waits 3s, then transforms next image
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {// need window!
+               title: "Credit Sequence!".into(),
+               present_mode: PresentMode::Fifo,
+               ..default() // Name and present mode all we need for now
+            }),
+            ..default()
+        }))
+        .add_systems(Startup,setup)
+        .add_system(Update, show_popup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {// first image to show
+        texture: asset_server.load("")
+        ..default()
+    });
+    commands
+        .spawn(SprintBundle {// second image
+            texture: asset_server.load(""),
+            transform: Transform::from_xyz(0.,0.,-1.),
+            ..default()
+        })
+        .insert(PopupTime(Timer::from_seconds(3.,TimerMode::Once)));
+    commands
+        .spawn(SprintBundle {// third image
+            texture: asset_server.load(""),
+            transform: Transform::from_xyz(0.,0.,-2.),
+            ..default()
+        })
+        .insert(PopupTime(Timer::from_seconds(6.,TimerMode::Once)));
+    commands
+        .spawn(SprintBundle {// fourth image
+            texture: asset_server.load(""),
+            transform: Transform::from_xyz(0.,0.,-3.),
+            ..default()
+        })
+        .insert(PopupTime(Timer::from_seconds(9.,TimerMode::Once)));
+    commands
+        .spawn(SprintBundle { // fifth Image
+            texture: asset_server.load(""),
+            transform: Transform::from_xyz(0.,0.,-4.),
+            ..default()
+        })
+        .insert(PopupTime(Timer::from_seconds(12.,TimerMode::Once)));
+    info!("Hello World!");
+}
+
+fn show_popup(time: Res<Time>, mut popup: Query<(&mut PopupTimer, &mut Transform)>){
+    for (mut timer, mut transform) in popup.iter_mut() {
+        timer.tick(time.delta());
+        if timer.just_finished(){
+            transform.translation *= -2;// stacked increasingly in -z, this pulls em back!
+            info!("Swapped pics!");
+        }
+
+    }
 }
