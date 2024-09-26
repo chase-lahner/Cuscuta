dc566d9f381f57b577b7cd8ccdb1682fd28ba54b
rcm71 <rcm71@pitt.edu>
2024-09-26 13:35:26 -0400

    running demo. need art

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {// need window!
-                title: "Credit Sequence!".into(),
+                title: TITLE.into(),
                 present_mode: PresentMode::Fifo,
                 ..default() // Name and present mode all we need for now
              }),
@@ -66,17 +66,163 @@ fn main() {
          }))
          .add_systems(Startup,setup)
          .add_systems(Update, move_player)
-         //.add_systems(Update, animate_player.after(move_player))
-         //.add_systems(Update, move_camera.after(animate_player))
+         .add_systems(Update, animate_player.after(move_player))
+         .add_systems(Update, move_camera.after(animate_player))
          .run();
 }
 
 fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    commands.spawn(Camera2dBundle::default());
 
-){}
+    let bg_texture_handle = asset_server.load("game_end_credit_screen_tyler.png");
 
-fn move_player(){}
+    let mut x_offset = 0.;
+    while x_offset < LEVEL_LEN {
+        commands
+            .spawn(SpriteBundle {
+                texture: bg_texture_handle.clone(),
+                transform: Transform::from_xyz(x_offset, 0., 0.),
+                ..default()
+            })
+            .insert(Background);
 
-fn animate_player(){}
+        x_offset += WIN_W;
+    }
 
-fn move_camera() {}
+    let player_sheet_handle = asset_server.load("walking.png");
+    let player_layout = TextureAtlasLayout::from_grid(UVec2::splat(TILE_SIZE), 4, 1, None, None);
+    let player_layout_len = player_layout.textures.len();
+    let player_layout_handle = texture_atlases.add(player_layout);
+    commands.spawn((
+        SpriteBundle {
+            texture: player_sheet_handle,
+            transform: Transform::from_xyz(0., -(WIN_H / 2.) + ((TILE_SIZE as f32) * 1.5), 900.),
+            ..default()
+        },
+        TextureAtlas {
+            layout: player_layout_handle,
+            index: 0,
+        },
+        AnimationTimer(Timer::from_seconds(ANIM_TIME, TimerMode::Repeating)),
+        AnimationFrameCount(player_layout_len),
+        Velocity::new(),
+        Player,
+    ));
+
+    let brick_sheet_handle = asset_server.load("bricks.png");
+    let brick_layout = TextureAtlasLayout::from_grid(UVec2::splat(TILE_SIZE), 4, 1, None, None);
+    let brick_layout_len = brick_layout.len();
+    let brick_layout_handle = texture_atlases.add(brick_layout);
+
+    let mut i = 0;
+    let mut t = Vec3::new(
+        -WIN_W / 2. + (TILE_SIZE as f32) / 2.,
+        -WIN_H / 2. + (TILE_SIZE as f32) / 2.,
+        0.,
+    );
+    while i * TILE_SIZE < (LEVEL_LEN as u32) {
+        commands.spawn((
+            SpriteBundle {
+                texture: brick_sheet_handle.clone(),
+                transform: Transform {
+                    translation: t,
+                    ..default()
+                },
+                ..default()
+            },
+            TextureAtlas {
+                layout: brick_layout_handle.clone(),
+                index: (i as usize) % brick_layout_len,
+            },
+            Brick,
+        ));
+
+        i += 1;
+        t += Vec3::new(TILE_SIZE as f32, 0., 0.);
+    }
+}
+fn move_player(
+    time: Res<Time>,
+    input: Res<ButtonInput<KeyCode>>,
+    mut player: Query<(&mut Transform, &mut Velocity), (With<Player>, Without<Background>)>,
+) {
+    let (mut pt, mut pv) = player.single_mut();
+
+    let mut deltav = Vec2::splat(0.);
+
+    if input.pressed(KeyCode::KeyA) {
+        deltav.x -= 1.;
+    }
+
+    if input.pressed(KeyCode::KeyD) {
+        deltav.x += 1.;
+    }
+    if input.pressed(KeyCode::KeyW){
+        deltav.y += 1.;
+    }
+    if input.pressed(KeyCode:: KeyS){
+        deltav.y -= 1.;
+    }
+
+    let deltat = time.delta_seconds();
+    let acc = ACCEL_RATE * deltat;
+
+    pv.velocity = if deltav.length() > 0. {
+        (pv.velocity + (deltav.normalize_or_zero() * acc)).clamp_length_max(PLAYER_SPEED)
+    } else if pv.velocity.length() > acc {
+        pv.velocity + (pv.velocity.normalize_or_zero() * -acc)
+    } else {
+        Vec2::splat(0.)
+    };
+    let change = pv.velocity * deltat;
+
+    let new_pos = pt.translation + Vec3::new(change.x, 0., 0.);
+    if new_pos.x >= -(WIN_W / 2.) + (TILE_SIZE as f32) / 2.
+        && new_pos.x <= LEVEL_LEN - (WIN_W / 2. + (TILE_SIZE as f32) / 2.)
+    {
+        pt.translation = new_pos;
+    }
+
+    let new_pos = pt.translation + Vec3::new(0., change.y, 0.);
+    if new_pos.y >= -(WIN_H / 2.) + ((TILE_SIZE as f32) * 1.5)
+        && new_pos.y <= WIN_H / 2. - (TILE_SIZE as f32) / 2.
+    {
+        pt.translation = new_pos;
+    }
+}
+
+fn animate_player(
+    time: Res<Time>,
+    mut player: Query<
+        (
+            &Velocity,
+            &mut TextureAtlas,
+            &mut AnimationTimer,
+            &AnimationFrameCount,
+        ),
+        With<Player>,
+    >,
+) {
+    let (v, mut texture_atlas, mut timer, frame_count) = player.single_mut();
+    if v.velocity.cmpne(Vec2::ZERO).any() {
+        timer.tick(time.delta());
+
+        // if timer.just_finished() {
+        texture_atlas.index = (texture_atlas.index + 1) % **frame_count;
+        // }
+    }
+}
+
+fn move_camera(
+    player: Query<&Transform, With<Player>>,
+    mut camera: Query<&mut Transform, (Without<Player>, With<Camera>)>,
+) {
+    let pt = player.single();
+    let mut ct = camera.single_mut();
+
+    ct.translation.x = pt.translation.x.clamp(0., LEVEL_LEN - WIN_W);
+}
