4ab6b91eb86ca1d09039e871abd73da5e0288cc2
chase-lahner <cal256@pitt.edu>
2024-10-09 23:50:54 -0400

    comments ig

--- src/main.rs
+++ src/main.rs
@@ -672,19 +672,18 @@ fn send_packet(
 }
 
 fn send_movement_info(
-    socket: Res<UDP>,
-    player: Query<&Transform, With<Player>>,
+    socket: Res<UDP>, // defined in setup
+    player: Query<&Transform, With<Player>>, // player transform
     
-) {
-    let pt = player.single();
+) { // consencus algs    let pt = player.single(); // get player transform
     let x = pt.translation.x;
     let y = pt.translation.y;
-    let xInt = unsafe {x.to_int_unchecked::<u8>()};
-    let yInt = unsafe {y.to_int_unchecked::<u8>()};
-    let buf:[u8;2] = [xInt, yInt];
+    let x_int = unsafe {x.to_int_unchecked::<u8>()}; // f32 to u8 WARNING: idk how dangerous this is but it's for sure unsafe :0
+    let y_int = unsafe {y.to_int_unchecked::<u8>()};
+    let buf:[u8;2] = [x_int, y_int]; // put it in u8 buffer format
     print!("{:?}", &buf);
 
-    socket.socket.send_to(&buf,"localhost:5001").unwrap();
+    socket.socket.send_to(&buf,"localhost:5001").unwrap();  // send to surver at lh 5001 unwrap is error handling
 
 }
 
