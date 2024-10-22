d2610464cce3cfcaf262ea622039067a2a10ec85
rcm71 <rcm71@pitt.edu>
2024-10-22 16:09:49 -0400

    push

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,11 @@
 use bevy::{prelude::*, window::PresentMode};
+use cuscuta_resources::TICKS_PER_SECOND;
 use library::*;
 
 fn main() {
     App::new()
         .insert_resource(room_gen::RoomManager::new())
+        .insert_resource(Time::<Fixed>::from_hz(TICKS_PER_SECOND))
         .add_systems(PreStartup, init::ip_setup) // should run before we spawn / send data to server
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
@@ -23,9 +25,6 @@ fn main() {
             player::move_player,
             player::player_input,
             player::update_player_position.after(player::player_input),
-            client::send_player.after(player::update_player_position),
-            client::listen,
-            client::send_player.after(client::listen),
             enemies::enemy_movement.after(player::move_player),
             player::animate_player.after(player::move_player),
             player::player_attack.after(player::animate_player),
@@ -34,6 +33,11 @@ fn main() {
             player::player_attack_enemy.after(player::player_attack)
             //player::player_interact,
         )) 
+        /* networking shtuff. comment out if needed */
+        .add_systems(FixedUpdate,
+            (client::send_player,
+            client::listen
+        ))
         //  player::player_interact)
         .run();
 }
